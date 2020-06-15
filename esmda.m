ac23b406e49d7232b1e34b18a3789ac7a0c92262
thiagoomenez <59297712+thiagoomenez@users.noreply.github.com>
2020-06-15 15:23:49 -0300

    Add files via upload

--- esmda.m
+++ esmda.m
@@ -1,7 +1,7 @@
 function [m] = esmda(Ne, m, d, alpha, Cd, dobs, Loc)
 
-Cmd = (1/Ne)*(m - mean(m, 2))*(d - mean(d, 2))';
-Cdd = (1/Ne)*(d - mean(d, 2))*(d - mean(d, 2))';
+Cmd = (1/(Ne-1))*(m - mean(m, 2))*(d - mean(d, 2))';
+Cdd = (1/(Ne-1))*(d - mean(d, 2))*(d - mean(d, 2))';
 K = Cmd/(Cdd + alpha*Cd);
 
 m = m + (Loc.*K)*(dobs + sqrt(alpha*Cd)*randn(Nd, 1) - d);
