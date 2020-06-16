6149f0db55ec908e0508ec14a3bb4f0b048d8503
thiagoomenez <59297712+thiagoomenez@users.noreply.github.com>
2020-06-16 06:56:28 -0300

    Update esmda.m

--- esmda.m
+++ esmda.m
@@ -1,9 +1,9 @@
-function [m] = esmda(Ne, m, d, alpha, Cd, dobs, Loc)
+function [m_a] = esmda(Ne, m_f, d, alpha, Cd, dobs, Loc)
 
-Cmd = (1/(Ne-1))*(m - mean(m, 2))*(d - mean(d, 2))';
+Cmd = (1/(Ne-1))*(m_f - mean(m_f, 2))*(d - mean(d, 2))';
 Cdd = (1/(Ne-1))*(d - mean(d, 2))*(d - mean(d, 2))';
 K = Cmd/(Cdd + alpha*Cd);
 
-m = m + (Loc.*K)*(dobs + sqrt(alpha*Cd)*randn(Nd, 1) - d);
+m_a = m_f + (Loc.*K)*(dobs + sqrt(alpha*Cd)*randn(Nd, 1) - d);
 
 end
