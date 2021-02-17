6984d72555c5deeeba06f9c797af176c6c739b78
thiagoomenez <59297712+thiagoomenez@users.noreply.github.com>
2021-02-16 22:38:12 -0300

    Update esmda.m

--- esmda.m
+++ esmda.m
@@ -4,6 +4,6 @@ Cmd = (1/(Ne-1))*(m_f - mean(m_f, 2))*(d - mean(d, 2))';
 Cdd = (1/(Ne-1))*(d - mean(d, 2))*(d - mean(d, 2))';
 K = Cmd/(Cdd + alpha*Cd);
 
-m_a = m_f + (Loc.*K)*(dobs + sqrt(alpha*Cd)*randn(Nd, 1) - d);
+m_a = m_f + (Loc.*K)*(dobs + sqrt(alpha*Cd).*randn(Nd, 1) - d);
 
 end
